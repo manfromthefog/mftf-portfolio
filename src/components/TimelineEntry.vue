46edeb48d6a0a2dac7c6152b85f4382ccbc4599a
Qinkai <77865332+manfromthefog@users.noreply.github.com>
2025-06-26 10:59:43 -0600

    Update TimelineEntry.vue

--- src/components/TimelineEntry.vue
+++ src/components/TimelineEntry.vue
@@ -2,10 +2,7 @@
 import { defineProps } from 'vue';
 
 const props = defineProps({
-  time: String,
-  event: String,
-  description: String,
-  images: Array,
+    timeline: Object,
 });
 </script>
 
@@ -19,17 +16,17 @@ const props = defineProps({
     <div class="ml-10 flex flex-col md:flex-row w-full items-center">
       <!-- Left: Text Content -->
       <div class="w-1/2 flex flex-col items-start">
-        <p class="inline-block px-2 py-1 mb-5 font-semibold text-white bg-stone-900 dark:bg-white dark:text-stone-900 rounded-md ">{{ time }}</p>
-        <h2 class="text-2xl font-semibold mb-2">{{ event }}</h2>
+        <p class="inline-block px-2 py-1 mb-5 font-semibold text-white bg-stone-900 dark:bg-white dark:text-stone-900 rounded-md ">{{ timeline.time }}</p>
+        <h2 class="text-2xl font-semibold mb-2">{{ timeline.event }}</h2>
         <div class="text-gray-900 text-justify pr-0 md:pr-8 lg:pr-16 text-lg">
           <slot name="description">
-            {{ description }}
+            {{ timeline.description }}
           </slot>
         </div>
       </div>
       <!-- Right: Images -->
       <div class="w-1/2 flex flex-wrap justify-center items-center md:justify-start gap-4 mt-4 md:mt-0">
-        <img v-for="(image, index) in images" :key="index" :src="image" alt="Timeline Image" class="w-32 h-32 object-cover rounded-md shadow-lg polaroid" />
+        <img v-for="(image, index) in timeline.images" :key="index" :src="image" alt="Timeline Image" class="w-32 h-32 object-cover rounded-md shadow-lg polaroid" />
       </div>
     </div>
   </div>
